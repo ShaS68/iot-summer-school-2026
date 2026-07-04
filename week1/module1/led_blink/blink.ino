525a88b2ccd80ff041597580b975933d60a2f380
ShaS68 <shahanass3722@gmail.com>
2026-07-04 19:34:00 +0530

    feat: add serial monitor output

--- week1/module1/led_blink/blink.ino
+++ week1/module1/led_blink/blink.ino
@@ -9,13 +9,17 @@ Basic LED Blink
 void setup()
 {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(13, HIGH);
+  Serial.println("LED ON");
   delay(500);
 
   digitalWrite(13, LOW);
+  Serial.println("LED OFF");
   delay(500);
 }
+
